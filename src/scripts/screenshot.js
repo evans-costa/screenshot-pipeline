c249080e42750c541b28a02875b215e92a9b67e9
Evandro Costa <evandro.souzac@gmail.com>
2024-01-08 23:39:51 -0300

    Remove IIFE and export named function for test use case

--- src/scripts/screenshot.js
+++ src/scripts/screenshot.js
@@ -1,17 +1,29 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: 'new',
-  });
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
-  await page.screenshot({
-    path: './images/screenshot.png',
-    fullPage: true,
-  });
-  await browser.close();
-  console.log('\n Screenshot captured successfully');
-})().catch((err) => {
-  console.error('Error while capturing screenshot: ', err.message);
-});
+async function screenshot() {
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({
+      headless: 'new',
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+    const image = await page.screenshot({
+      path: './images/screenshot.png',
+      fullPage: true,
+    });
+    console.log('\n Screenshot captured successfully');
+    return image;
+  } catch (err) {
+    console.error('Error while capturing screenshot: ', err.message);
+  } finally {
+    await browser.close();
+  }
+}
+
+if (require.main === module) {
+  screenshot();
+}
+
+module.exports = screenshot;
