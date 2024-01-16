0eff9ff33a26fa16844125351eeac869bca72b96
Evandro Costa <evandro.souzac@gmail.com>
2024-01-16 15:45:02 -0300

    Change initial script when running test

--- src/tests/jest-puppeteer.config.js
+++ src/tests/jest-puppeteer.config.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  server: process.env.BASE_URL
-    ? undefined
-    : {
-        command: `npm run start`,
-        launchTimeout: 30000,
-        debug: true,
-      },
+  server: {
+    command: 'npm run dev',
+    launchTimeout: 30000,
+    port: 3000,
+    debug: true,
+  },
   launch: {
     headless: 'new',
     ignoreDefaultArgs: ['--disable-extensions'],
