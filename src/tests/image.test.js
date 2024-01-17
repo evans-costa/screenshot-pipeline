bc9b0c94b6dbe45cd2d02a339a3daee2d0f68ebf
Evandro Costa <evandro.souzac@gmail.com>
2024-01-17 18:34:20 -0300

    Change thereshold

--- src/tests/image.test.js
+++ src/tests/image.test.js
@@ -5,19 +5,17 @@ function getConfig() {
   const jestImageSnapshot = path.join(__dirname, '..', '..', 'images');
 
   return {
-    comparisonMethod: 'ssim',
-    diffDirection: 'vertical',
     customSnapshotsDir: jestImageSnapshot,
     customSnapshotIdentifier: 'visual-regression-diff',
     updatePassedSnapshot: true,
-    failureThreshold: 0.05,
+    failureThreshold: 0.1,
     allowSizeMismatch: true,
     failureThresholdType: 'percent',
   };
 }
 
 describe('Visual Regression Test', () => {
-  it('should pass if there is less than 1% difference', async () => {
+  it('should pass if there is less than 10% difference', async () => {
     await page.goto(baseURL);
 
     const image = await page.screenshot({ fullPage: true });
